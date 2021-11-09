f6a4bfcbe81bb2e98457fb330aabb7669aa9c156
seboschtion <10785615+seboschtion@users.noreply.github.com>
2021-11-09 17:15:33 +0100

    simplified winner.py

--- README.md
+++ README.md
@@ -10,3 +10,20 @@ Clone the repository, change into its directory and run the following commands:
 2. `source .venv/bin/activate`
 3. `pip install -r requirements.txt`
 4. `python -m ctf`
+
+## How It Works
+
+### General Advise
+
+- If teams are involved in the event, always evaluate teams first (use the `--teams` option).
+- Winners are stored into the `memory.ctf` file so that they cannot win again.
+
+### Algorithm
+
+1. Fetch all participants and organize them into teams, if they are team members.
+2. Get solutions, and out of it, the challenges.
+3. Add potential winners to the `candidates` field of a challenge.
+4. Remove all previous winners (stored in `memory.ctf`) from the `candidates` field.
+5. Select winner for challenge.
+6. Add winners to `memory.ctf` file.
+7. Print challenges and their winners.
--- ctf/winner.py
+++ ctf/winner.py
@@ -1,40 +1,40 @@
 import shelve
-from typing import List, Any, Set
+from typing import List, Any, Set, Union
 from random import choice
 from ctf.model.Challenge import Challenge
 from ctf.model.User import User
+from ctf.model.Team import Team
 
 
 def select_winners(
     challenges: List[Challenge], teams_only: bool, users: List[User]
 ) -> List[Challenge]:
-    clean_challenges = _remove_previous_winners(challenges, teams_only)
+    challenges_with_possible_winners = _remove_previous_winners(challenges, teams_only)
     round_winners = set()
-    for challenge in clean_challenges:
+    for challenge in challenges_with_possible_winners:
         candidates = list(challenge.candidates - round_winners)
         winner = choice(candidates) if candidates else None
         challenge.winner = winner
         round_winners.add(winner)
-    winners = []
+    winners = _get_winners(challenges_with_possible_winners)
     if teams_only:
-        winner_teams = set(
-            map(
-                lambda c: c.winner, filter(lambda c: c.winner != None, clean_challenges)
-            )
-        )
-        winner_user_ids = [
-            x
-            for sublist in list(map(lambda w: w.member_ids, winner_teams))
-            for x in sublist
-        ]
+        winner_user_ids = []
+        for winner_team in winners:
+            winner_user_ids += winner_team.member_ids
         winners = filter(lambda u: u.id in winner_user_ids, users)
-    else:
-        winners = map(
-            lambda c: c.winner, filter(lambda c: c.winner != None, clean_challenges)
-        )
 
     _remember_winners(list(winners))
-    return clean_challenges
+    return challenges_with_possible_winners
+
+
+def _get_winners(
+    challenges: List[Challenge],
+) -> Set[Union[Team, User]]:
+    winners = set()
+    for challenge in challenges:
+        if challenge.winner != None:
+            winners.add(challenge.winner)
+    return winners
 
 
 def _get_previous_winners() -> List[User]:
@@ -57,7 +57,6 @@ def _remove_previous_winners(
         return challenges
     previous_winner_teams = set(map(lambda w: w.team, previous_winners))
     for challenge in challenges:
-        invalid_winners = []
         if teams_only:
             invalid_winners = previous_winner_teams & challenge.candidates
         else:
